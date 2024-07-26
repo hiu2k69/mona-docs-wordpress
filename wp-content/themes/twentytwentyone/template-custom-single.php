1806556bc432f05632c8a6c22b52440aee9c759f
Nguyen Minh Hieu <55779953+hieudev69@users.noreply.github.com>
2024-07-26 17:47:22 +0700

    up

--- wp-content/themes/twentytwentyone/template-custom-single.php
+++ wp-content/themes/twentytwentyone/template-custom-single.php
@@ -111,29 +111,7 @@ h2.name-title.ml-5 {
     }
 }
 
-@media only screen and (max-width: 991px) {
-    h2.name-title.ml-5 {
-        font-size: 13px; /* Adjust as needed */
-    }
-    .container.box-last- {
-        margin-left: 15%; /* Adjust as needed */
-    }
-    .tab-content.mt-5 {
-        width: 90%; /* Adjust as needed */
-        margin-top: 4rem; /* Adjust as needed */
-    }
-    .col-lg-9.col-md-9 {
-        margin-left: 15%;
-        padding-left: 10px;
-    }
-    .gap-05 {
-        gap: 0.5rem;
-        padding: 4px 40px; /* Adjust as needed */
-    }
-    .col-lg-2.col-md-3 {
-        display: none;
-    }
-}
+
 
 
 .box-bottom.d-flex.mt-2 {
@@ -637,7 +615,7 @@ h2.name-title.ml-5 {
             </div>
 
          </div>
-            <div class="col-lg-9 col-md-12 col-12">
+            <div class="col-lg-9 col-md-9">
                 <div class="tab-content mt-5">
                     <?php
 
